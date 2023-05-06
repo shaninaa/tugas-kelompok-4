6daa9bbc54e1a46190c9e0eba0129445b8bff760
shaninaa <shaninaprasesti@gmail.com>
2023-05-07 00:29:53 +0700

    Update hakAksesController.php

--- app/Http/Controllers/hakAksesController.php
+++ app/Http/Controllers/hakAksesController.php
@@ -45,6 +45,28 @@ class HakAksesController extends Controller
     return redirect('/hakakses')->with('success', 'Data berhasil ditambahkan!');
     }
 
+    public function editHakAkses($id_akses)
+    {
+        $hakakses = DB::table('hak_akses')->where('id_akses', $id_akses)->get();
+        $data = array(
+            'menu' => 'hakakses',
+            'submenu' => 'hakakses',
+            'hakakses' => $hakakses
+            );
+        return view('admin.updatehakakses',  $data);
+    }
+
+    public function updateHakAkses(Request $post)
+    {
+        DB::table('hak_akses')->where('id_akses', $post->id_akses)->update([
+			'id_akses' => $post->id_akses,
+            'nama_akses' => $post->nama_akses,
+            'keterangan' => $post->keterangan,
+        ]);
+
+        return redirect('/hakakses')->with('success', 'Data berhasil diupdate!');
+    }
+
     public function delete($id_akses){
         $hakakses = hakakses::where('id_akses', $id_akses)->first();
         $hakakses->delete();
